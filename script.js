927678b3da3391be80e38560285dcbae2a42317e
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-05-20 15:40:11 +0530

    feat:which button is active

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("script attached")
+console.log("script attached");
+
+const canvas = document.getElementById("drawingCanvas");
+let activeTool = "pencilButton";
+
+function setupCanvas() {
+  const react = canvas.getBoundingClientRect();
+  const dpi = window.devicePixelRatio;
+  canvas.width = react.width * dpi;
+  canvas.height = react.height * dpi;
+  console.log("canvas", canvas);
+}
+
+setupCanvas();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toolbar = document.getElementById("toolbar");
+  toolbar.addEventListener("click", (event) => {
+    if (activeTool) {
+      document.getElementById(activeTool).classList.remove("active");
+    }
+    activeTool = event.target.id;
+    const clickedTool = document.getElementById(event.target.id);
+    clickedTool.classList.add("active");
+  });
+});
